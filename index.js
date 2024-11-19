3d9c9759b980aaf8917e3fd05bb968135fc1fb5f
Alex12Rodrigues <alex.rodoliveira2@gmail.com>
2024-11-19 20:35:19 -0300

    Fazendo chat no MongoDB

---
+++ index.js
@@ -0,0 +1,82 @@
+/* Importando pacotes:
+        -express
+        -ejs
+        -http
+        -path
+        -socket.io
+*/
+
+const express = require('express');
+const ejs = require('ejs'); 
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+const { Socket } = require('dgram');
+
+
+/* 
+    Instancias:
+        -express
+        -server
+        -socket.io
+        */
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+
+/* 
+    Define a localização da pasta estática:
+    */
+
+app.use(express.static(path.join(__dirname, "public")))
+
+
+/* 
+    Define o EJS como a engine de rendereização frontend:
+    */
+app.set("views", path.join(__dirname, "public"));
+app.engine("html", ejs.renderFile)
+
+/* 
+    Rota raiz '/' para acessar o index.html da aplicação:
+    */
+app.use('/', (req, resp) => {
+    resp.render("index.html")
+})
+
+
+/* 
+    Início do código do chat 
+    */
+
+
+/* 
+    Array que armazena as mensagens
+    */
+let messages = [];
+
+
+/*
+    Cria a conexão com socket.io
+    */
+io.on('connection', socket =>{
+    console.log("Novo usuário conectado! ID: " +socket.id)
+});
+
+
+/* 
+    Fim do código do chat
+    */
+
+
+/* 
+    criando servidor https:
+    */
+
+server.listen(3000, () =>{
+    console.log("Servidor do web chat rodando em http://localhost:3000")
+});
+
+
